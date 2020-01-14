2b4c9a6744931099460a09a098744f971998c41f
Éric D <15271+edas@users.noreply.github.com>
2020-01-14 22:04:30 +0100

    fix 5

--- src/build.js
+++ src/build.js
@@ -2,6 +2,8 @@ const fs = require('fs')
 const path = require('path')
 const jsonc = require('jsonc')
 
+const param = process.argv[2]
+
 const manifestPath = path.join('.', 'package.json')
 const manifestContent = fs.readFileSync(manifestPath)
 const manifest = jsonc.parse(manifestContent)
@@ -9,20 +11,29 @@ const manifest = jsonc.parse(manifestContent)
 const safeName = manifest.name.replace(/\W+/g, '-').replace(/^-|-$/g, '')
 const licenseUrl = `https://spdx.org/licenses/${manifest.license}.html#licenseText`
 
+const destDir = path.join('.', 'dist')
+const srcDir = path.join('.', 'src')
+
 const now = new Date()
 
 // consent-o-matic
 async function consentomatic() {
 	const root = 'consent-o-matic'
-	const src = 'rules.d'
 	const ext = '.json'
 
-	const rootDir = path.join('.', root)
-	const srcDir = path.join(rootDir, src)
-	const files = await fs.promises.readdir(srcDir)
-	const contents = files.map(
+	const rootDir = path.join(srcDir, root)
+	const files = await fs.promises.readdir(rootDir)
+	const filteredFiles = files.filter(
+		file => {
+			if (file[0] === '.') return false
+			if (path.extname(file) !== ext) return false
+			if (param && file !== (param + ext)) return false
+			return true
+		}
+	)
+	const contents = filteredFiles.map(
 		async function(file) {
-			const filePath = path.join(srcDir, file)
+			const filePath = path.join(rootDir, file)
 			const content = await fs.promises.readFile(filePath)
 			const data = jsonc.parse(content.toString())
 			return data
@@ -31,14 +42,13 @@ async function consentomatic() {
 	const dataList = await Promise.all(contents)
 	const data = Object.assign({}, ...dataList)
 	const content = jsonc.stringify(data, null, 4)
-	const destFileName = safeName + '--' + root + ext
-	const destFilePath = path.join(rootDir, destFileName)
+	const destFileName = root + ext
+	const destFilePath = path.join(destDir, destFileName)
 	await fs.promises.writeFile(destFilePath, content)
 }
 
 async function ublockorigin() {
 	const root = 'ublock-origin'
-	const src = 'filters.d'
 	const ext = '.txt'
 	const header = `
 ! Title: ${manifest.name} filters
@@ -53,12 +63,19 @@ async function ublockorigin() {
 ! Last updated: ${now.toISOString()}
 `.replace(/^\r?\n/mg, '')
 	
-	const rootDir = path.join('.', root)
-	const srcDir = path.join(rootDir, src)
-	const files = await fs.promises.readdir(srcDir)
-	const contents = files.map(
+	const rootDir = path.join(srcDir, root)
+	const files = await fs.promises.readdir(rootDir)
+	const filteredFiles = files.filter(
+		file => {
+			if (file[0] === '.') return false
+			if (path.extname(file) !== ext) return false
+			if (param && file !== (param + ext)) return false
+			return true
+		}
+	)
+	const contents = filteredFiles.map(
 		async function(file) {
-			const filePath = path.join(srcDir, file)
+			const filePath = path.join(rootDir, file)
 			const content = await fs.promises.readFile(filePath)
 			const sectionHeader = `! *** ${filePath} ***\n`
 			const data = (sectionHeader + content + "\n").replace(/(\r?\n)+$/, "\n")
@@ -67,8 +84,8 @@ async function ublockorigin() {
 	)
 	const dataList = await Promise.all(contents)
 	const content = [header, ...dataList].join("\n")
-	const destFileName = safeName + '--' + root + ext
-	const destFilePath = path.join(rootDir, destFileName)
+	const destFileName = root + ext
+	const destFilePath = path.join(destDir, destFileName)
 	await fs.promises.writeFile(destFilePath, content)
 }
 
